9f8134c490deb2e4ca4761cf80798b3beff1e1f8
Anyll Markevich <anyllmarkevich@gmail.com>
2025-12-31 01:00:41 -0700

    Algorythm successfully plays an optimal game of mastermind against itself, using a significant optimization to reduce computation time on the first guess.

--- R_Version/optimal_mastermind_main.R
+++ R_Version/optimal_mastermind_main.R
@@ -18,32 +18,8 @@ o_space <- space
 password <- space[sample(1:(c^n),1),]
 password
 
-
-find_guess_structure <- function(guesses) {
-  print(guesses)
-  guess_info <- matrix(nrow = length(guesses[,1]), ncol = length(unique(guesses[1,])))
-  print(length(unique(guesses[1,])))
-  for (i in 1:length(guesses[,1])) {
-    u <- unique(guesses[i,])
-    n_occurences <- c()
-    for (j in u) {
-      n_occurences <- c(n_occurences, length(which(guesses[i,]==j)))
-    }
-    guess_info[i,] <- sort(n_occurences)
-    #tryCatch(guess_info[i,] <- sort(n_occurences), finally = stop("Not all guesses were equivalent."))
-  }
-  for (i in 2:length(guess_info[,1])) {
-    if (!all(guess_info[i,]==guess_info[i-1,])) {
-      stop("Not all guesses were equivalent.")
-    }
-  }
-  print("Structure:")
-  print(guess_info[1,])
-  #return(guess_info[1,])
-}
-
 combo_to_guesses <- function(combos) {
-  guesses <- matrix(nrow = 0, ncol = sum(combos[1,]))
+  guesses <- matrix(nrow = 0, ncol = sum(combos[[1]]))
   for (combo in combos) {
     guess <- c()
     color <- 1
@@ -53,40 +29,45 @@ combo_to_guesses <- function(combos) {
     }
     guesses <- rbind(guesses, guess)
   }
+  rownames(guesses) <- NULL
   return(guesses)
 }
 
-possible_combos <- function(n_colors, n_pins) {
+possible_combos <- function(n_pins) {
+  new_combos <- list(c(rep(1, n_pins)))
   combos <- list()
-  for (i in 1:floor(n_pins/2)) {
-    temp <- c(i, rep(1, n_pins-i))
-    #print(temp)
-    combos <- append(combos, list(sort(temp)))
-  }
-  #print(combos)
-  for (rep in 1:(n_pins-1)) {
-    for (i in combos) {
-      if (1 %in% i) {
-        n_ones <- length(which(i==1))
-        for (j in 1:n_ones) {
+  temp_combos <- list(NA)
+  while (length(temp_combos)>0) {
+    temp_combos <- list()
+    for (i in new_combos) {
+      n_ones <- length(which(i==1))
+      if (n_ones > 1) {
+        for (j in 2:n_ones) {
           temp <- c( i[which(i!=1)], j)
           if (j < n_ones) {
-            temp <- c(temp, rep(1, (n_ones-j) ) )
+            temp <- c(temp, rep(1, (n_ones-j)))
           }
-          #print(i)
-          #print(temp)
-          #print("")
-          combos <- append(combos, list(sort(temp)))
+          temp_combos <- append(temp_combos, list(sort(temp)))
         }
       }
     }
+    combos <- append(combos, new_combos)
+    new_combos <- unique(temp_combos)
   }
-  #print(combos)
-  print("finished")
+  combos <- append(combos, new_combos)
   return(unique(combos))
 }
-possible_combos(2, 6)
 
+valid_combos <- function(n_pins, n_cols) {
+  valid_combs <- list()
+  possible_combs <- possible_combos(n_pins)
+  for (i in possible_combs) {
+    if (length(i) <= n_cols) {
+      valid_combs <- append(valid_combs, list(i))
+    }
+  }
+  return(valid_combs)
+}
 
 get_response <- function(guess, pass) {
   half_pins <- 0
@@ -124,22 +105,14 @@ info_of_guess <- function(guess, space) {
   }
   u <- unique(responses)
   t_info <- 0
-  #print("Probabilities")
-  #temp <- c()
   for (i in 1:length(u[,1])) {
     pi <- length(which(responses[,1]==u[i,1] & responses[,2]==u[i,2]))/length(responses[,1])
-    #temp <- c(temp, pi)
-    #print(pi)
     t_info <- t_info - pi*log2(pi)
   }
-  #print("sum")
-  #print(sum(temp))
-  #print(t_info)
-  
   return(t_info)
 }
 
-select_best_guess <- function(space, o_space, iter) {
+select_best_guess <- function(space, o_space) {
   info <- c()
   cli_progress_bar("Selecting Best Guess", total = length(o_space[,1]))
   for (i in 1:length(o_space[,1])) {
@@ -147,38 +120,32 @@ select_best_guess <- function(space, o_space, iter) {
     cli_progress_update()
   }
   cli_progress_done()
-  #print("Info")
-  #print(info)
-  #print("max")
-  #print(max(info))
-  #print("Which")
-  #print(as.double(info)==as.double(4.052252))
-  #print(str(info[722]))
-  #print(info==max(info))
-  #print(which(info==max(info)))
-  #print(which.max(info))
-  #print(info==4.052252)
-  #print(round(info*1000000)==max(round(info*1000000)))
-  #print("Values")
-  #print(o_space[which(info==max(info)),])
-  #print(round(info, digits=6))
-  #print(round(info, digits=6)==max(round(info, digits=6)))
-  #print("Values")
-  #print(o_space[which(round(info, digits=6)==max(round(info, digits=6))),])
-  if (iter==0) {
-    print("First Guess Info")
-    find_guess_structure(o_space[which(round(info, digits=6)==max(round(info, digits=6))),])
-    print("End First Guess Info")
+  correct_values <- which(round(info, digits=6)==max(round(info, digits=6)))
+  print("ospace")
+  print(o_space)
+  print(info)
+  print(which(round(info, digits=6)==max(round(info, digits=6))))
+  print("guesslist")
+  print(o_space[which(round(info, digits=6)==max(round(info, digits=6))),])
+  print(length(o_space[,1]))
+  print(o_space[sample(which(round(info, digits=6)==max(round(info, digits=6))), 1),,drop=FALSE])
+  if (length(correct_values) > 1) {
+    return(o_space[sample(correct_values, 1),]) 
+  } else {
+    return(o_space[correct_values[1],])
   }
-  return(o_space[sample(which(round(info, digits=6)==max(round(info, digits=6))), 1),])
 }
-
-#select_best_guess(space, o_space)
 
 guess <- c(0,0,0)
 it <- 0
+guess <- select_best_guess(space, combo_to_guesses(valid_combos(n, c)))
+response <- get_response(guess, password)
+space <- trim_space(guess, response, space)
+it <- it + 1
+print("First Guess")
+print(guess)
 while (!all(guess == password) && length(space[,1])>1) {
-  guess <- select_best_guess(space, o_space, it)
+  guess <- select_best_guess(space, o_space)
   #print("Guess")
   #print(guess)
   #print("Space")
@@ -193,9 +160,3 @@ print("Password")
 password
 print("Iterations")
 it
-
-
-
-
-
-
