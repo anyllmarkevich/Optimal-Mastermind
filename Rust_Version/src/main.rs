a0201a8f7f4d6e6fab2af6d35b8336dbb1ebdbc3
Anyll Markevich <anyllmarkevich@gmail.com>
2026-01-05 15:14:08 -0700

    Added code to see how long a run took for performance monitoring.

--- Rust_Version/src/main.rs
+++ Rust_Version/src/main.rs
@@ -1,4 +1,5 @@
 use optimal_mastermind::{Row, Space};
+use std::time::Instant;
 
 const N_COLORS: u8 = 5;
 const N_PINS: u8 = 5;
@@ -7,12 +8,15 @@ fn main() {
     let mut space = Space::build(N_COLORS, N_PINS);
     let password = space.choose_password();
     let mut guess: Row;
+    let start_time = Instant::now();
     loop {
         guess = space.run_turn(&password);
         println!("Guess was: {:?}", guess.get_vals());
         if space.get_space_size() <= 1 {
             break;
         }
+        let duration = start_time.elapsed();
+        println!("Time elapsed: {:?}", duration);
     }
     println!("Password was: {:?}", &password.get_vals());
 }
